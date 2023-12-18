07e4cf0b0eeb13016cbd6fa2925f4f90cc4c7acb
Zerom (Junho Jin) <111134273+jincode93@users.noreply.github.com>
2023-12-18 15:30:45 +0900

    CardOnFileDashboard Riblet(슈퍼페이 잔고) 및 하위 View 추가

---
+++ MiniSuperApp/FinanceHome/CardOnFileDashboard/CardOnFileDashboardBuilder.swift
@@ -0,0 +1,38 @@
+//
+//  CardOnFileDashboardBuilder.swift
+//  MiniSuperApp
+//
+//  Created by Zerom on 2023/12/18.
+//
+
+import ModernRIBs
+
+protocol CardOnFileDashboardDependency: Dependency {
+    var cardOnFileRepository: CardOnFileRepository { get }
+}
+
+final class CardOnFileDashboardComponent: Component<CardOnFileDashboardDependency>, CardOnFileDashboardInteractorDependency {
+    var cardOnFileRepository: CardOnFileRepository { dependency.cardOnFileRepository }
+}
+
+protocol CardOnFileDashboardBuildable: Buildable {
+    func build(withListener listener: CardOnFileDashboardListener) -> CardOnFileDashboardRouting
+}
+
+final class CardOnFileDashboardBuilder: Builder<CardOnFileDashboardDependency>, CardOnFileDashboardBuildable {
+
+    override init(dependency: CardOnFileDashboardDependency) {
+        super.init(dependency: dependency)
+    }
+
+    func build(withListener listener: CardOnFileDashboardListener) -> CardOnFileDashboardRouting {
+        let component = CardOnFileDashboardComponent(dependency: dependency)
+        let viewController = CardOnFileDashboardViewController()
+        let interactor = CardOnFileDashboardInteractor(
+            presenter: viewController,
+            dependency: component
+        )
+        interactor.listener = listener
+        return CardOnFileDashboardRouter(interactor: interactor, viewController: viewController)
+    }
+}
---
+++ MiniSuperApp/FinanceHome/CardOnFileDashboard/CardOnFileDashboardInteractor.swift
@@ -0,0 +1,61 @@
+//
+//  CardOnFileDashboardInteractor.swift
+//  MiniSuperApp
+//
+//  Created by Zerom on 2023/12/18.
+//
+
+import Combine
+import ModernRIBs
+
+protocol CardOnFileDashboardRouting: ViewableRouting {
+}
+
+protocol CardOnFileDashboardPresentable: Presentable {
+    var listener: CardOnFileDashboardPresentableListener? { get set }
+    
+    func update(with viewModels: [PaymentMethodViewModel])
+}
+
+protocol CardOnFileDashboardListener: AnyObject {
+}
+
+protocol CardOnFileDashboardInteractorDependency {
+    var cardOnFileRepository: CardOnFileRepository { get }
+}
+
+final class CardOnFileDashboardInteractor: PresentableInteractor<CardOnFileDashboardPresentable>, CardOnFileDashboardInteractable, CardOnFileDashboardPresentableListener {
+
+    weak var router: CardOnFileDashboardRouting?
+    weak var listener: CardOnFileDashboardListener?
+
+    private let dependency: CardOnFileDashboardInteractorDependency
+    
+    private var cancellables: Set<AnyCancellable>
+    
+    init(
+        presenter: CardOnFileDashboardPresentable,
+        dependency: CardOnFileDashboardInteractorDependency
+    ) {
+        self.dependency = dependency
+        self.cancellables = .init()
+        super.init(presenter: presenter)
+        presenter.listener = self
+    }
+
+    override func didBecomeActive() {
+        super.didBecomeActive()
+        
+        dependency.cardOnFileRepository.cardOnFile.sink { methods in
+            let viewModels = methods.prefix(5).map(PaymentMethodViewModel.init)
+            self.presenter.update(with: viewModels)
+        }.store(in: &cancellables)
+    }
+
+    override func willResignActive() {
+        super.willResignActive()
+        
+        cancellables.forEach { $0.cancel() }
+        cancellables.removeAll()
+    }
+}
