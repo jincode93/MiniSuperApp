7ccfa949e1e73d40dec94f414e60dcb1eaa40e6e
Zerom (Junho Jin) <111134273+jincode93@users.noreply.github.com>
2023-12-21 16:40:28 +0900

    기본 설정값을 저정할 DefaultStore 추가 - 추후 열림, 닫힘 원칙을 적용해서 리팩토링 필요

--- Platform/Package.swift
+++ Platform/Package.swift
@@ -16,6 +16,15 @@ let package = Package(
         .library(
             name: "SuperUI",
             targets: ["SuperUI"]),
+        .library(
+            name: "DefaultsStore",
+            targets: ["DefaultsStore"]),
+        .library(
+            name: "Network",
+            targets: ["Network"]),
+        .library(
+            name: "NetworkImp",
+            targets: ["NetworkImp"]),
     ],
     dependencies: [
         .package(url: "https://github.com/CombineCommunity/CombineExt", from: "1.0.0"),
@@ -38,6 +47,22 @@ let package = Package(
             name: "SuperUI",
             dependencies: [
                 "RIBsUtil"
+            ]
+        ),
+        .target(
+            name: "DefaultsStore",
+            dependencies: [
+            ]
+        ),
+        .target(
+            name: "Network",
+            dependencies: [
+            ]
+        ),
+        .target(
+            name: "NetworkImp",
+            dependencies: [
+                "Network"
             ]
         ),
     ]
---
+++ Platform/Sources/DefaultsStore/DefaultsStore.swift
@@ -0,0 +1,43 @@
+//
+//  DefaultsStore.swift
+//  MiniSuperApp
+//
+//  Created by Zerom on 2023/12/21.
+//
+
+import Foundation
+
+public protocol DefaultsStore {
+    var isInitialLaunch: Bool { get set }
+    var lastNoticeDate: Double { get set }
+}
+
+public struct DefaultsStoreImp: DefaultsStore {
+    
+    public var isInitialLaunch: Bool {
+        get {
+            userDefaults.bool(forKey: kIsInitialLaunch)
+        }
+        set {
+            userDefaults.set(newValue, forKey: kIsInitialLaunch)
+        }
+    }
+    
+    public var lastNoticeDate: Double {
+        get {
+            userDefaults.double(forKey: kLastNoticeDate)
+        }
+        set {
+            userDefaults.set(newValue, forKey: kLastNoticeDate)
+        }
+    }
+    
+    private let userDefaults: UserDefaults
+    
+    private let kIsInitialLaunch = "kIsInitialLaunch"
+    private let kLastNoticeDate = "kLastNoticeDate"
+    
+    public init(defaults: UserDefaults) {
+        self.userDefaults = defaults
+    }
+}
